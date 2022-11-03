77361bbe697c4410748c0380e7c6ad494ea730b3
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2022-11-04 02:41:25 +0530

    first commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    public  static int STATIC_TIMER =2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, STATIC_TIMER);
+    }
+}
